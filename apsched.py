647c3fcc6f0d7f81c731d435fdd83dcc2d308c6e
Mihail <103327413+Kitores@users.noreply.github.com>
2024-09-03 10:59:43 +0300

    Update apsched.py

--- apsched.py
+++ apsched.py
@@ -3,9 +3,9 @@ from time import sleep
 async def send_message_cron(bot: Bot):
     for i in range(10):
         sleep(5)
-        await bot.send_message(874999696, "kjvnkjndl")
+        await bot.send_message("id", "hello")
 
 async def send_message_time(bot: Bot):
     for i in range(10):
         sleep(5)
-        await  bot.send_message(874999696, "41412421")
+        await  bot.send_message("id", "hi")
